ff90107a13f02bf20bd1e3e2b8a0e72b5c048aa6
O-S-H <dhtjdgus153@naver.com>
2022-01-16 19:38:38 +0900

    save

---
+++ Assets/Script/assignment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int gold = 1000;
+        gold = gold - 200; //800
+
+        float itemWeight = 1.34f;
+        bool isStroreOpen = true;
+        string name = "Æ÷¼Ç";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
