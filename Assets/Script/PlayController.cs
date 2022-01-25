de1bc2974ab9735295d90535f101f8f4180f5585
O-S-H <dhtjdgus153@naver.com>
2022-01-25 17:34:18 +0900

    save 1- 25복습
    save 1- 25복습

--- Assets/Script/PlayController.cs
+++ Assets/Script/PlayController.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class PlayController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
     void Start()
     {
+        playerRigidbody = GetComponent<Rigidbody>();
         
     }
+    void Update()
+    {
 
-    
-}
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+
+        }
+
+
+
+
+
+
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }    
+
+} 
