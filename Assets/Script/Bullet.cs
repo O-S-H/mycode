e8c9ff42eb961e53e79c2238f429d697476cb668
O-S-H <dhtjdgus153@naver.com>
2022-01-29 13:17:33 +0900

    1-29 복습
    283 -288

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -17,7 +17,18 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject, 3f);
         
     }
+   void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayController playController = other.GetComponent<PlayController>();
 
+            if(playController != null) //컴포넌트를 가져오는데 성공했다면
+            {
+                playController.Die();
+            }
+        }
+    }
     // Update is called once per frame 
     void Update()
     {
