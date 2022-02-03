03079fea3457e23d6084d4cd23f67dae3f7ac03f
O-S-H <dhtjdgus153@naver.com>
2022-02-03 21:10:28 +0900

    2-3 복습
    복습

---
+++ Assets/Script/BulletSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletprefab; //생성할 탄알의 원본 프로팹
+    public float spwnRateMin = 0.5f;// 최소 생성 주기 
+    public float spwnRateMax = 3f; //쵀대성성주기
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterspwn;
+
+
+    void Start()
+    {
+        timeAfterspwn = 0f;
+        spawnRate = Random.Range(spwnRateMin, spwnRateMax);
+        target = FindObjectOfType<PlayController>().transform;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
